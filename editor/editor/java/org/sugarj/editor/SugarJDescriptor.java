59e8457eb6030070540f956815191ec6a8fe2fda
LennartKats <LennartKats@ba653a0a-7fb7-0310-a048-973b18a224e4>
2011-02-23 16:49:56 +0000

    Editor services?
    git-svn-id: https://svn.strategoxt.org/repos/StrategoXT/sugarj/branches/SugarJ-editor-services@22109 ba653a0a-7fb7-0310-a048-973b18a224e4

---
+++ editor/editor/java/org/sugarj/editor/SugarJDescriptor.java
@@ -0,0 +1,64 @@
+package org.sugarj.editor;
+
+import static org.spoofax.interpreter.core.Tools.termAt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.imp.language.ILanguageService;
+import org.spoofax.interpreter.terms.IStrategoAppl;
+import org.spoofax.interpreter.terms.IStrategoConstructor;
+import org.spoofax.interpreter.terms.IStrategoList;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.spoofax.interpreter.terms.ITermFactory;
+import org.spoofax.terms.StrategoListIterator;
+import org.strategoxt.imp.runtime.Environment;
+import org.strategoxt.imp.runtime.dynamicloading.BadDescriptorException;
+import org.strategoxt.imp.runtime.dynamicloading.Descriptor;
+import org.strategoxt.imp.runtime.parser.SGLRParseController;
+
+/**
+ * @author Lennart Kats <lennart add lclnet.nl>
+ */
+public class SugarJDescriptor extends Descriptor {
+
+  private final IStrategoAppl baseDocument;
+
+  public SugarJDescriptor(Descriptor baseDescriptor) throws BadDescriptorException {
+    super(baseDescriptor.getDocument());
+    baseDocument = baseDescriptor.getDocument();
+  }
+  
+  @Override
+  public synchronized <T extends ILanguageService> T createService(
+      Class<T> type, SGLRParseController controller)
+      throws BadDescriptorException {
+    
+    if (controller.getParser() instanceof SugarJParser) {
+      List<IStrategoTerm> services = ((SugarJParser) controller.getParser()).getEditorServices();
+      setDocument(composeDefinitions(baseDocument, services));
+      simpleClearCache(controller); // TODO: only clear when necessary
+    }
+    
+    return super.createService(type, controller);
+  }
+  
+  private static IStrategoAppl composeDefinitions(IStrategoAppl base, List<IStrategoTerm> extensions) {
+    IStrategoConstructor cons = base.getConstructor();
+    if (cons.getName().equals("Module") && cons.getSubtermCount() == 3) {
+      ITermFactory factory = Environment.getTermFactory();
+      List<IStrategoTerm> allDefinitions = new ArrayList<IStrategoTerm>();
+      addAll(allDefinitions, (IStrategoList) termAt(base, 2));
+      allDefinitions.addAll(extensions);
+      return factory.makeAppl(cons, termAt(base, 0), factory.makeList(allDefinitions));
+    } else {
+      throw new IllegalStateException("Unsupported editor descriptor format:" + cons);
+    }
+  }
+
+  private static void addAll(List<IStrategoTerm> all, IStrategoList list) {
+    for (IStrategoTerm term : StrategoListIterator.iterable(list)) {
+      all.add(term);
+    }
+  }
+}
