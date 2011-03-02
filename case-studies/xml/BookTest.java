a1ba44f6bc4b4dacf0cc481c99bf2b4c19b5a087
Sebastian Erdweg <seba.github@gmail.com>
2011-03-02 09:32:55 +0000

    improved XML case study
    git-svn-id: https://svn.strategoxt.org/repos/StrategoXT/sugarj/branches/SugarJ-editor-services@22244 ba653a0a-7fb7-0310-a048-973b18a224e4

--- case-studies/xml/BookTest.java
+++ case-studies/xml/BookTest.java
@@ -1,8 +1,8 @@
 package xml;
-
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
-import xml.XmlAsSax;
+import xml.AsSax;
+import xml.Test;
 import xml.XmlJavaSyntax;
 public class BookTest extends xml.Test 
 { 
@@ -12,14 +12,18 @@ public class BookTest extends xml.Test
     String title = "Sweetness and Power: The Place of Sugar in Modern History";
     { 
       ch.startDocument();
-      org.xml.sax.helpers.AttributesImpl b_0 = new org.xml.sax.helpers.AttributesImpl();
-      b_0.addAttribute("", "title", "title", "CDATA", title + "");
-      ch.startElement("", "book", "book", b_0);
-      org.xml.sax.helpers.AttributesImpl a_0 = new org.xml.sax.helpers.AttributesImpl();
-      a_0.addAttribute("", "name", "name", "CDATA", "Sidney W. Mintz" + "");
-      ch.startElement("", "author", "author", a_0);
+      org.xml.sax.helpers.AttributesImpl k_3 = new org.xml.sax.helpers.AttributesImpl();
+      k_3.addAttribute("", "title", "title", "CDATA", title + "");
+      ;
+      ch.startElement("", "book", "book", k_3);
+      org.xml.sax.helpers.AttributesImpl j_3 = new org.xml.sax.helpers.AttributesImpl();
+      j_3.addAttribute("", "name", "name", "CDATA", "Sidney W. Mintz" + "");
+      ;
+      ch.startElement("", "author", "author", j_3);
       ch.endElement("", "author", "author");
+      ;
       ch.endElement("", "book", "book");
+      ;
       ch.endDocument();
     }
   }
