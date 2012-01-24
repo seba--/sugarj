b37bc90bd3739576bf0bba74e8f209fb57a2ed13
Sebastian Erdweg <seba.github@gmail.com>
2012-01-24 14:44:23 +0100

    generate constructor signatures from model

--- compiler/org/sugarj/driver/Driver.java
+++ compiler/org/sugarj/driver/Driver.java
@@ -62,7 +62,7 @@ import org.sugarj.util.ToplevelDeclarationProvider;
  */
 public class Driver{
   
-  public final static String CACHE_VERSION = "model-0.1b";
+  public final static String CACHE_VERSION = "model-0.1c";
   
   private final static int PENDING_TIMEOUT = 30000;
 
