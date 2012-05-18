393805e902033a02b2aac57e9ac36594fa0c7baf
Sebastian Erdweg <seba.github@gmail.com>
2012-05-18 13:37:13 +0200

    avoid using JSGLRI, use SGLR directly instead

--- editor/editor/java/org/sugarj/editor/SugarJParser.java
+++ editor/editor/java/org/sugarj/editor/SugarJParser.java
@@ -23,9 +23,11 @@ import org.spoofax.interpreter.terms.IStrategoString;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.jsglr.client.InvalidParseTableException;
 import org.spoofax.jsglr.client.KeywordRecognizer;
+import org.spoofax.jsglr.client.SGLR;
 import org.spoofax.jsglr.client.imploder.IToken;
 import org.spoofax.jsglr.client.imploder.Token;
 import org.spoofax.jsglr.client.imploder.Tokenizer;
+import org.spoofax.jsglr.client.imploder.TreeBuilder;
 import org.spoofax.jsglr.shared.BadTokenException;
 import org.spoofax.jsglr.shared.SGLRException;
 import org.spoofax.jsglr.shared.TokenExpectedException;
@@ -306,8 +308,7 @@ public class SugarJParser extends JSGLRI {
           new AbsolutePath(langLib.getInitTrans().getPath()),
           "main",
           new LinkedList<Path>(),
-          new JSGLRI(org.strategoxt.imp.runtime.Environment
-              .loadParseTable(StdLib.strategoTbl.getPath()), "StrategoModule"),
+          new SGLR(new TreeBuilder(), org.strategoxt.imp.runtime.Environment.loadParseTable(StdLib.strategoTbl.getPath())),
           org.strategoxt.strj.strj.init(), strCache, environment, langLib);
     } catch (InvalidParseTableException e) {
       throw new RuntimeException(e);
