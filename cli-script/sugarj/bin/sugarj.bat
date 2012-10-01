939617ecb7617374ae9f24cffd2f410229cd4f8b
Cai Yufei <0.yfcai@gmail.com>
2012-10-01 16:57:32 +0200

    Unignore cli-script/sugarj/bin

---
+++ cli-script/sugarj/bin/sugarj.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+SETLOCAL enabledelayedexpansion
+
+set WORKDIR=%CD%
+
+
+cd /d "%0%\.."
+
+set SCRIPT=%CD%
+
+
+cd %WORKDIR%
+
+
+java -Xss64m -Xmx1024m -cp "%SCRIPT%\..\lib" org.sugarj.driver.cli.Main %*%
+endlocal
---
+++ cli-script/sugarj/bin/sugh.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+
+SETLOCAL enabledelayedexpansion
+
+set WORKDIR=%CD%
+
+
+cd /d "%0%\.."
+
+
+set SCRIPT=%CD%
+
+
+cd %WORKDIR%
+
+
+"%SCRIPT%\sugarj.bat" -l haskell %*%
+endlocal
