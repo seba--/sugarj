897fa77bfb113935265e00fefab75deb0b6adcb9
Sebastian Erdweg <seba.github@gmail.com>
2013-05-14 18:37:12 +0200

    accept strategies with term arguments

--- common/src/org/sugarj/common/typesmart/WithoutTypesmartSyntax.java
+++ common/src/org/sugarj/common/typesmart/WithoutTypesmartSyntax.java
@@ -34,13 +34,25 @@ public class WithoutTypesmartSyntax extends AbstractPrimitive {
     }
   }
   
-  public static IStrategoTerm invoke(Context ctx, org.strategoxt.lang.Strategy s, IStrategoTerm current) {
+  public static IStrategoTerm invoke(Context ctx, org.strategoxt.lang.Strategy s, IStrategoTerm current, IStrategoTerm... targs) {
     ITermFactory oldFactory = ctx.getFactory();
     try {
       if (oldFactory instanceof TypesmartTermFactory) {
         ctx.setFactory(((TypesmartTermFactory) ctx.getFactory()).getBaseFactory());
       }
-      return s.invoke(ctx, current);
+      if (targs.length == 0)
+        return s.invoke(ctx, current);
+      if (targs.length == 1)
+        return s.invoke(ctx, current, targs[0]);
+      if (targs.length == 2)
+        return s.invoke(ctx, current, targs[0], targs[1]);
+      if (targs.length == 3)
+        return s.invoke(ctx, current, targs[0], targs[1], targs[2]);
+      if (targs.length == 4)
+        return s.invoke(ctx, current, targs[0], targs[1], targs[2], targs[3]);
+      if (targs.length == 5)
+        return s.invoke(ctx, current, targs[0], targs[1], targs[2], targs[3], targs[4]);
+      throw new IllegalArgumentException("Too many term arguments provided: " + targs);
     } finally {
       ctx.setFactory(oldFactory);
     }
